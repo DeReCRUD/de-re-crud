4165bec6d352138c71bee37f9f957861037c0158
Greg Beaty <gregbeatyjr@gmail.com>
2018-08-24 19:10:48 -0400

    Fix CSS import replacement script

---
+++ scripts/replace-dist-css-imports.js
@@ -0,0 +1,15 @@
+const replace = require('replace-in-file');
+
+const options = {
+  files: 'dist/**/*.js',
+  from: /import \'(.*).css\';/g,
+  to: '',
+  verbose: true
+};
+
+const changes = replace.sync(options);
+if (!changes.length) {
+  console.warn('CSS Import Replacement: No files were modified.');
+} else{
+  console.log(changes.map(x => `CSS Import Replacement: ${x}`).join('\n'));
+}
