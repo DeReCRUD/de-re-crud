d61856cb08059d28330c036b9d1d4d0a78868498
Greg Beaty <gregbeatyjr@gmail.com>
2018-12-03 00:12:15 -0500

    Fix tests

--- packages/core/validators/__tests__/required-validator.test.ts
+++ packages/core/validators/__tests__/required-validator.test.ts
@@ -4,25 +4,26 @@ import RequiredValidator from '../required-validator';
 describe('RequiredValidator', () => {
   let sut: RequiredValidator = null;
 
-  const field: IField = {
-    keyField: false,
-    struct: 'struct',
-    name: 'field',
-    label: {
-      short: 'Field',
-      medium: 'Field',
-      long: 'Field'
-    },
-    type: 'text',
-    required: true,
-    unique: false,
-    hints: {
-      width: 1
-    }
-  };
+  let field: IField = null;
 
   beforeEach(() => {
     sut = new RequiredValidator();
+    field = {
+      keyField: false,
+      struct: 'struct',
+      name: 'field',
+      label: {
+        short: 'Field',
+        medium: 'Field',
+        long: 'Field'
+      },
+      type: 'text',
+      required: true,
+      unique: false,
+      hints: {
+        width: 1
+      }
+    };
   });
 
   it('should return true if field is linked struct', () => {
@@ -38,15 +39,15 @@ describe('RequiredValidator', () => {
   });
 
   it('should return false if field is null', () => {
-    expect(sut.validate(field, null)).toBe(true);
+    expect(sut.validate(field, null)).toBe(false);
   });
 
   it('should return false if field is undefined', () => {
-    expect(sut.validate(field, undefined)).toBe(true);
+    expect(sut.validate(field, undefined)).toBe(false);
   });
 
   it('should return false if field is empty string', () => {
-    expect(sut.validate(field, '')).toBe(true);
+    expect(sut.validate(field, '')).toBe(false);
   });
 
   it('should return true if field is 0', () => {
