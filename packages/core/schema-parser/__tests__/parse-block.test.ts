e3554ee3a9abad2e5da222a73a1d5d820b806e91
Greg Beaty <gregbeatyjr@gmail.com>
2019-02-10 02:25:16 -0500

    Add tests

--- packages/core/schema-parser/__tests__/parse-block.test.ts
+++ packages/core/schema-parser/__tests__/parse-block.test.ts
@@ -1,4 +1,4 @@
-import { IInternalBlock } from '../../internal-schema';
+import { IInternalBlock, IInternalStamp } from '../../internal-schema';
 import parseBlock from '../parse-block';
 import { DEFAULT_CONDITION } from '../parse-condition';
 
@@ -29,5 +29,112 @@ describe('parseBlock', () => {
 
   it('should return defaults when no overrides specified', () => {
     expect(parseBlock(structName, blockJson)).toEqual(block);
+  });
+
+  it('should parse stamp with default size', () => {
+    blockJson = {
+      ...blockJson,
+      fields: [
+        {
+          stamp: 'Stamp',
+          size: 3,
+          condition: DEFAULT_CONDITION,
+        },
+      ],
+    };
+
+    expect(parseBlock(structName, blockJson)).toEqual({
+      ...block,
+      items: [
+        {
+          text: 'Stamp',
+          size: 3,
+          blockInstance: 1,
+          condition: DEFAULT_CONDITION,
+          hints: {
+            custom: {},
+          },
+        } as IInternalStamp,
+      ],
+    } as IInternalBlock);
+  });
+
+  it('should parse stamp with specified size', () => {
+    blockJson = {
+      ...blockJson,
+      fields: [
+        {
+          stamp: 'Stamp',
+          size: 1,
+          condition: DEFAULT_CONDITION,
+        },
+      ],
+    };
+
+    expect(parseBlock(structName, blockJson)).toEqual({
+      ...block,
+      items: [
+        {
+          text: 'Stamp',
+          size: 1,
+          blockInstance: 1,
+          condition: DEFAULT_CONDITION,
+          hints: {
+            custom: {},
+          },
+        } as IInternalStamp,
+      ],
+    } as IInternalBlock);
+  });
+
+  it('should parse simple field references', () => {
+    blockJson = {
+      ...blockJson,
+      fields: ['field1'],
+    };
+
+    const fields = [
+      {
+        field: 'field1',
+        condition: DEFAULT_CONDITION,
+        hints: {
+          custom: {},
+        },
+      },
+    ];
+
+    expect(parseBlock(structName, blockJson)).toEqual({
+      ...block,
+      fields,
+      items: fields,
+    } as IInternalBlock);
+  });
+
+  it('should parse complex field references', () => {
+    blockJson = {
+      ...blockJson,
+      fields: [
+        {
+          field: 'field1',
+          condition: DEFAULT_CONDITION,
+        },
+      ],
+    };
+
+    const fields = [
+      {
+        field: 'field1',
+        condition: DEFAULT_CONDITION,
+        hints: {
+          custom: {},
+        },
+      },
+    ];
+
+    expect(parseBlock(structName, blockJson)).toEqual({
+      ...block,
+      fields,
+      items: fields,
+    } as IInternalBlock);
   });
 });
