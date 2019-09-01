2c66b134a4c5d18ab4700b4043f4a7170ea40295
Greg Beaty <gregbeatyjr@gmail.com>
2019-09-01 01:33:46 -0400

    Replace babel with ts-jest for testing

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  projects: ['<rootDir>/packages'],
+  resetMocks: true,
+  testURL: 'http://localhost',
+  moduleFileExtensions: ['ts', 'tsx', 'js'],
+  testRegex: '(/__tests__/.*\\.(test|spec))\\.(js|jsx|ts|tsx)?$',
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  collectCoverageFrom: [
+    'packages/**/*.{ts,js}',
+    '!packages/**/rollup*',
+    '!packages/**/*.d.ts',
+    '!packages/storybook/**/*',
+  ],
+};
