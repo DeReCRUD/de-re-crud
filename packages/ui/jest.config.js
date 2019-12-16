7f40e28f20c316380ae6c9663f845a1f23859456
Greg Beaty <gregbeatyjr@gmail.com>
2019-12-16 15:30:15 -0500

    Add additional libraries for testing

--- packages/ui/jest.config.js
+++ packages/ui/jest.config.js
@@ -2,6 +2,7 @@ const baseConfig = require('../../jest.config.base');
 
 module.exports = {
   ...baseConfig,
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   moduleNameMapper: {
     '@de-re-crud/(.*)': '<rootDir>/../../packages/$1/src',
   },
