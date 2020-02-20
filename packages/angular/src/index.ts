69601b72e986c6debf13950ec0d5f154b522038e
Greg Beaty <gregbeatyjr@gmail.com>
2020-02-20 00:08:37 -0500

    Export JSX host component/directive
    This should resolve the ivy compiler issues.

--- packages/angular/src/index.ts
+++ packages/angular/src/index.ts
@@ -1,4 +1,6 @@
 export * from './form.component';
+export * from './jsx-host.component';
+export * from './jsx-host.directive';
 export * from './form-submission';
 export * from './lib.module';
 export * from './renderer';
